005ddb8f534f7b612f5e81720cbcaf8b7d9662c4
unknown <aastha.mail97@gmail.com>
2023-01-11 20:39:08 +0530

    story 3: Add a text to a new file, function added

--- main.go
+++ main.go
@@ -9,15 +9,14 @@ import (
 
 func main() {
 
-	fmt.Println("Choose one:/n 1. search string /n 2. sub string ")
+	fmt.Println("Choose one:\n 1. search string \n 2. sub string \n 3. Write text in a new file ")
 	var option int
 	fmt.Scanln(&option)
-
+	var str, fileName string
+	fmt.Scan(&str, &fileName)
 	switch option {
 	case 1:
-		var searchStr, fileName string
-		fmt.Scan(&searchStr, &fileName)
-		result, err := searchString(searchStr, fileName)
+		result, err := searchString(str, fileName)
 
 		if err == nil {
 			fmt.Println(result)
@@ -25,9 +24,15 @@ func main() {
 			fmt.Println(err)
 		}
 	case 2:
-		var searchStr, fileName string
-		fmt.Scan(&searchStr, &fileName)
-		result, err := searchSubString(searchStr, fileName)
+		result, err := searchSubString(str, fileName)
+
+		if err == nil {
+			fmt.Println(result)
+		} else {
+			fmt.Println(err)
+		}
+	case 3:
+		result, err := addStrToNewFile(str, fileName)
 
 		if err == nil {
 			fmt.Println(result)
@@ -84,4 +89,17 @@ func searchSubString(searchStr, fileName string) ([]string, error) {
 	}
 
 	return list, err
+}
+
+func addStrToNewFile(newString, fileName string) (string, error) {
+	content, err := os.Open(fileName)
+	var warning string = fileName + " file already exists"
+	if err != nil {
+		content, _ := os.Create(fileName)
+		content.Write([]byte("You have added the text " + newString))
+		fileScanner := bufio.NewScanner(content)
+		return fileScanner.Text(), nil
+	}
+	defer content.Close()
+	return warning, err
 }
